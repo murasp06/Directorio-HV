c4a26ea90400ab99cc1083dc5ad7fa49f4abe9b4
murasp06 <104362346+murasp06@users.noreply.github.com>
2023-10-14 02:14:09 +0000

    correccion de enlaces de cabecera sin loguear

--- model/config.general.php
+++ model/config.general.php
@@ -1,10 +1,10 @@
 <?php 
 // ruta inicial de la carpeta del proyecto
-$rutaraiz='/workspaces/PWA_AMFICA_V.0.1/';
-$ruta='https://friendly-capybara-wqpwv5rppjrfxgp-8000.app.github.dev/sessiones/';
+$rutaraiz='/';
+$ruta='../sessiones/';
 
 // ruta para quitar el ciclo de busqueda del enlace
-$ruta1='https://friendly-capybara-wqpwv5rppjrfxgp-8000.app.github.dev/Views/';
+$ruta1='../Views/';
 
 
 ?>
