1f49b6e137e4c9ee42a1129537353dd3e658bd52
murasp06 <104362346+murasp06@users.noreply.github.com>
2023-10-14 12:55:33 +0000

    correcion en nombre de bd para conexion

--- model/conexion_bd.php
+++ model/conexion_bd.php
@@ -1,7 +1,7 @@
 <?php
 
 $servidor="app-03fdae2e-66d6-43e9-9c9a-a5d44d83e8c2-do-user-14624445-0.b.db.ondigitalocean.com"; //aqui va el ip o nombre del dominio
-$bd="amfica-db?sslmode=require"; //nombre de bases de datos
+$bd="amfica-db"; //nombre de bases de datos
 $usuario="amfica-db"; //va el usuario para bd
 $pass="AVNS_jYehb3WG8oe0PJ_S_3f"; //clave para bd
 
