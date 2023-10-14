81dae2dfa4396574e3a2b5199a839e8805c9cf9b
murasp06 <104362346+murasp06@users.noreply.github.com>
2023-10-14 03:44:20 +0000

    agregamos los meta autor,descricion,keyword

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="amfica,AMFICA,amigos fieles,amigos fieles dejando huella de cariño">
+    <meta name="description" content="Este aplicativo tendra lafuncion de guardar toda la informacion de sus mascotas con el objetivo de resguardar el carnet, prestar diferentes sevicios como directorio especializado para mascotas">
+    <meta name="author" content="Murasp06">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <title>AMFICA!</title>
