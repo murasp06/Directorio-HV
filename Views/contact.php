559651bade591b48ad120f8fb16a227ef018dc60
murasp06 <104362346+murasp06@users.noreply.github.com>
2023-10-14 12:26:23 +0000

    se agrego el fsvicon a lapg contacto y se trato de corregir la conexin a la bd

--- Views/contact.php
+++ Views/contact.php
@@ -9,6 +9,7 @@
     <title>AMFICA!</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="shortcut icon" href="../favicon/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../image/WhatsApp Image 2023-10-01 at 5.45.47 PM.jpeg" type="image/x-icon/png">
     <style>
         body {
             font-family: 'Times New Roman', Times, serif;
--- model/conexion_bd.php
+++ model/conexion_bd.php
@@ -1,9 +1,9 @@
 <?php
 
-$servidor=""; //aqui va el ip o nombre del dominio
-$bd=""; //nombre de bases de datos
-$usuario=""; //va el usuario para bd
-$pass=""; //clave para bd
+$servidor="app-03fdae2e-66d6-43e9-9c9a-a5d44d83e8c2-do-user-14624445-0.b.db.ondigitalocean.com:25060"; //aqui va el ip o nombre del dominio
+$bd="amfica-db?sslmode=require"; //nombre de bases de datos
+$usuario="amfica-db"; //va el usuario para bd
+$pass="AVNS_jYehb3WG8oe0PJ_S_3f"; //clave para bd
 
 try{
     $con=new PDO("mysql:host=$servidor;dbname=$bd",$usuario,$pass);
