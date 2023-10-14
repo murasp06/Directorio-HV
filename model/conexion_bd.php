55a1b5fa554cca10101f2e1ce28c77ef012398f7
murasp06 <104362346+murasp06@users.noreply.github.com>
2023-10-14 13:35:50 +0000

    cambio de db de dgo a ccloud

--- model/conexion_bd.php
+++ model/conexion_bd.php
@@ -1,12 +1,12 @@
 <?php
 
-$servidor="app-03fdae2e-66d6-43e9-9c9a-a5d44d83e8c2-do-user-14624445-0.b.db.ondigitalocean.com"; //aqui va el ip o nombre del dominio
-$bd="amfica-db"; //nombre de bases de datos
-$usuario="amfica-db"; //va el usuario para bd
-$pass="AVNS_jYehb3WG8oe0PJ_S_3f"; //clave para bd
+$servidor="bg5td4muhosvsmmrv8yt-mysql.services.clever-cloud.com"; //aqui va el ip o nombre del dominio
+$bd="bg5td4muhosvsmmrv8yt"; //nombre de bases de datos
+$usuario="uhfduzfmyczxvdbh"; //va el usuario para bd
+$pass="4W8o2OwZ7muiYw8MuLci"; //clave para bd
 
 try{
-    $con=new PDO("pgsql:host=$servidor;dbname=$bd;port=25060",$usuario,$pass);
+    $con=new PDO("mysql:host=$servidor;dbname=$bd;port=25060",$usuario,$pass);
 }catch(Exception $ex){
     echo "error de conexion ".$ex->getMessage();
 }
