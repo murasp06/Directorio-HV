63812951f3d3ad21a593b36fd77375167924cfe2
murasp06 <104362346+murasp06@users.noreply.github.com>
2023-10-15 17:08:30 +0000

    buscando falla

--- controlador/con.login.php
+++ controlador/con.login.php
@@ -18,10 +18,11 @@ if (empty($_POST['user']) and empty($_POST['clave'])) {
     foreach ($registros as $dato);
     //$rol=$dato->id_rol;
     if($dato->id_rol == 5 AND $dato->activo == 1 ) {    
-      session_start();
-      $_SESSION['id']=$dato->id_usuario;
-      $_SESSION['usuario']=$dato->nombre." ".$dato->apellidos;
-      header("location: ../Views/user.php");
+      // session_start();
+      // $_SESSION['id']=$dato->id_usuario;
+      // $_SESSION['usuario']=$dato->nombre." ".$dato->apellidos;
+      // header("location: ../Views/user.php");
+      echo "Funciona ";
       
     }elseif($dato->id_rol == 1 AND $dato->activo == 11){
       session_start();
