20cbc5ab7407b987d49d05fa28d20bb1eee9f431
murasp06 <104362346+murasp06@users.noreply.github.com>
2023-10-28 17:57:18 -0500

    Update con.insert.php
    se cambia el codigo para que registre el usuario debidamente

--- controlador/con.insert.php
+++ controlador/con.insert.php
@@ -1,33 +1,36 @@
 <?php
-require('../model/conexion_bd.php');
-
 
 // $id_usuario = $_POST['id_usuario'];
 $user = $_POST['user'];
 $clave = $_POST['clave'];
+$clave1 = $_POST['clave1'];
 $nombre = $_POST['nombre'];
-$apellidos= $_POST['apellidos'];
+$apellidos = $_POST['apellidos'];
 $edad = $_POST['edad'];
-$documento= $_POST['documento'];
-$ciudad= $_POST['ciudad'];
+$documento = $_POST['documento'];
+$ciudad = $_POST['ciudad'];
 $num_celular = $_POST['num_celular'];
-$correo= $_POST['correo'];
-$direccion= $_POST['direccion'];
+$correo = $_POST['correo'];
+$direccion = $_POST['direccion'];
+
 // $foto_user=$_POST['foto_user'];
 
-$sql =$con->prepare("INSERT INTO usuarios (user,clave,nombre,apellidos,edad,documento,ciudad,num_celular,correo,direccion,id_rol,activo) 
-VALUES ('$user','$clave','$nombre','$apellidos','$edad','$documento','$ciudad','$num_celular','$correo','$direccion',DEFAULT,DEFAULT)");
-$sql->execute();
+if ($clave == $clave1) {
+    require('../model/conexion_bd.php');
+    $sql = $con->prepare("INSERT INTO usuarios (user,clave,nombre,apellidos,edad,documento,ciudad,num_celular,correo,direccion,id_rol,activo) 
+     VALUES ('$user','$clave1','$nombre','$apellidos','$edad','$documento','$ciudad','$num_celular','$correo','$direccion',DEFAULT,DEFAULT)");
+    $sql->execute();
 
-if($sql){
-    header("location: ../Views/User.php");
-}else{
-    echo 'no registro..';
+    if ($sql) {
+        header("location: ../views/login.php?id=01");
+    } else {
+        echo '<div style="background-color: rgba(240, 44, 44, 0.277);color: red;padding:1px;">Usuario no registrado intente de nuevo en 30seg!</div>';
+    }
+} else {
+    header("location: ../views/registros.php?id=02");
+    //echo '<div style="background-color: rgba(240, 44, 44, 0.277);color: red;padding:1px;">Las contaseñas no son iguales!</div>';
 }
-
- //cerrar la conexion a la bd.
+//cerrar la conexion a la bd.
 // para cerrar la conexion a la bd se asigna null a la variable que construye el pdo
-$sql=null;
-
-
+ $sql=null;
 ?>
