f32ce2af833df4feb4a4f8f6373c88a0c56eec78
murasp06 <104362346+murasp06@users.noreply.github.com>
2023-11-11 22:11:48 -0500

    Update con.insertmas.php
    se corrigio la registrada de las mascotas

--- controlador/con.insertmas.php
+++ controlador/con.insertmas.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
+session_start();
 $varsesion = $_SESSION['usuario'];
+$id_usuario = $_SESSION['id'];
 if ($varsesion == null || $varsesion == '') {
     // echo 'usted no tiene autorizacion';
     header("location: ../index.php");
     die();
 }
-
-
 
 $nombre_mascota = $_POST['nombre_mascota'];
 // $doc_animal = $_POST['doc_animal'];
@@ -15,15 +15,12 @@ $especie = $_POST['especie'];
 $raza = $_POST['raza'];
 $sexo = $_POST['sexo'];
 $fecha_nacimiento = $_POST['fecha_nacimiento'];
-$id_usuario =  $_SESSION['id'];
+// $id_usuario =  $_SESSION['id'];
 $foto_mascota = $_FILES['foto_mascota'];
-
-// guardada de fotos
 $nombre_imagen = $_FILES['foto_mascota']['name'];
 $tipo_imagen = $_FILES['foto_mascota']['type'];
 $tamano_imagen = $_FILES['foto_mascota']['size'];
-// echo $nombre_imagen;
-
+// $fotomas='';
 if (isset($_FILES['foto_mascota'])) {
     $check = getimagesize($_FILES["foto_mascota"]["tmp_name"]);
     if ($check !== false) {
@@ -32,12 +29,11 @@ if (isset($_FILES['foto_mascota'])) {
         if ($tamano_imagen <= 3000000) {
             if ($tipo_imagen == "image/jpeg" || $tipo_imagen == "image/jpg" || $tipo_imagen == "image/png" || $tipo_imagen == "image/gif") {
                 require('../model/conexion_bd.php');
-            
                 // echo $id_usuario, $cc_animal, $especie, $fecha_nacimiento, $foto_mascota, $raza, $sexo, $nombre_mascota;
-                $sql =$con->prepare("INSERT INTO mascotas (nombre_mascota,doc_animal,especie,raza,sexo,fecha_nacimiento,id_usuario,foto_mascota)
+                $sql =$con->prepare( "INSERT INTO mascotas (nombre_mascota,doc_animal,especie,raza,sexo,fecha_nacimiento,id_usuario,foto_mascota)
                 VALUES ('$nombre_mascota',default,'$especie','$raza','$sexo','$fecha_nacimiento','$id_usuario','$foto_mascotaConten');");
                 $sql->execute();
-                if ($query) {
+                if ($sql) {
                     // echo '<script> document.write("registro exitoso"); </script>';
                     header("location: ../views/usuarios.php");
                 } else {
@@ -50,12 +46,11 @@ if (isset($_FILES['foto_mascota'])) {
     }
 } else {
     require('../model/conexion_bd.php');
-   
     // echo $id_usuario, $cc_animal, $especie, $fecha_nacimiento, $foto_mascota, $raza, $sexo, $nombre_mascota;
     $sql =$con->prepare("INSERT INTO mascotas (nombre_mascota,doc_animal,especie,raza,sexo,fecha_nacimiento,id_usuario,)
-   VALUES ('$nombre_mascota',default,'$especie','$raza','$sexo','$fecha_nacimiento','$id_usuario',);");
+   VALUES ('$nombre_mascota',default,'$especie','$raza','$sexo','$fecha_nacimiento','$id_usuario',)");
     $sql->execute();
-    if ($query) {
+    if ($sql) {
         // echo '<script> document.write("registro exitoso"); </script>';
         header("location: ../views/usuarios.php");
     } else {
@@ -63,8 +58,8 @@ if (isset($_FILES['foto_mascota'])) {
     }
 }
 
-//cerrar la conexion a la bd.
-// para cerrar la conexion a la bd se asigna null a la variable que construye el pdo
-$sql=null;
 
+
+//cerrar la conexion a la bd.
+$sql=null;
 ?>
